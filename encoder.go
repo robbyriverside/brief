765a52e3008e90d04426f15741bed2e1e122acbd
Rob Farrow <rob@fibberist.com>
2021-05-20 09:18:56 -0700

    iterative encoder

--- encoder.go
+++ encoder.go
@@ -3,36 +3,52 @@ package brief
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 // Encode converts a node into brief format
 func (node *Node) Encode() []byte {
 	var out strings.Builder
-
-	node.writeLine(&out)
+	body := node.write(&out)
+	for len(body) > 0 {
+		next := body[0]
+		body = body[1:]
+		more := next.write(&out)
+		body = append(more, body...)
+	}
 
 	return []byte(out.String())
 }
 
-func (node *Node) writeLine(out *strings.Builder) {
+func isIdentRune(ch rune, i int) bool {
+	return ch == '_' || unicode.IsLetter(ch) || unicode.IsDigit(ch) && i > 0
+}
+
+func isSymbol(token string) bool {
+	for i, ch := range token {
+		if !isIdentRune(ch, i) {
+			return false
+		}
+	}
+	return true
+}
+
+func (node *Node) write(out *strings.Builder) []*Node {
 	indent := strings.Repeat(" ", node.Indent)
 	out.WriteString(indent + node.Type)
 	if len(node.Name) > 0 {
 		out.WriteString(":" + node.Name)
 	}
 	for key, val := range node.Keys {
-		if strings.ContainsAny(val, ": \"//") {
-			out.WriteString(fmt.Sprintf(" %s:%q", key, val))
+		if isSymbol(val) {
+			out.WriteString(fmt.Sprintf(" %s:%s", key, val))
 			continue
 		}
-		out.WriteString(fmt.Sprintf(" %s:%s", key, val))
+		out.WriteString(fmt.Sprintf(" %s:%q", key, val))
 	}
 	if len(node.Content) > 0 {
 		out.WriteString(fmt.Sprintf(" `%s`", node.Content))
 	}
 	out.WriteString("\n")
-
-	for _, sub := range node.Body {
-		sub.writeLine(out)
-	}
+	return node.Body
 }
--- encoder_test.go
+++ encoder_test.go
@@ -1,6 +1,7 @@
 package brief_test
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 
@@ -8,11 +9,20 @@ import (
 )
 
 func TestEncoder(t *testing.T) {
-	// FIXME: validate results
+	t.Logf("\n%s", test0)
 	nodes, err := brief.Decode(strings.NewReader(test0))
 	if err != nil {
 		t.Fatal(err)
 	}
-	out := nodes[0].Encode()
-	t.Logf("%s", string(out))
+	before := nodes[0]
+	out := before.Encode()
+	t.Logf("\n%s", string(out))
+	nodes, err = brief.Decode(strings.NewReader(string(out)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	after := nodes[0]
+	if !reflect.DeepEqual(before, after) {
+		t.Fatal("before and after not the same")
+	}
 }
